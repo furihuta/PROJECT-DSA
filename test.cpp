ded0c121577528574563351cd458ff914a1459f4
thuan65 <58379769+thuan65@users.noreply.github.com>
2025-03-08 17:07:48 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
-//ey thay toi
-//fagfwqgqwg
-//fuck u
-//fuck u
+
 using namespace std;
 //see me?
 // My name is Van
 //do you see me
 // Biến đếm số lần so sánh
 long long comp_count = 0;
+
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if(left < n && arr[left] > arr[largest])
+        largest = left;
+    
+    if(right < n && arr[right] > arr[largest])    
+        largest = right;
+
+    if(++comp_count && largest != i) {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+
+    for(int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+
+}
 
 void bubble_sort(vector<int>& arr) {
     int n = arr.size();
